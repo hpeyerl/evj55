27f8a17c12ac4ccc65366e7d2db6cee522a645eb
Herb Peyerl <hpeyerl+gitlab@beer.org>
2025-05-09 07:55:56 -0600

    Start documenting our HF1A tcase wiring

---
+++ wiring/hf1a_tcase.cpp
@@ -0,0 +1,17 @@
+// P.1019  fj_hdj_hdj-80_series_chassis.pdf
+connectors:
+#include "connectors/hf1a_tcase.yml"
+#include "connectors/hf1a_tcase_relay.yml"
+
+cables:
+  hf1a_4wd_actuator:
+    wirecount: 4
+    colors: [ GNBU, GNBK, GNYE, GN ]
+    // [ 2, 5, 6, 3 ]
+
+  transfer_control_relay:
+    wirecount: 9
+    colors: [ GNBU, GNBK, GNYE, GN, WHBK, BKRD, BK, BKBU, BUBK ]
+    // [ 4, 10, 5, 1, 2, 6, 9, 7, 3 ]
+
+// connections:
